5a024ced43db2b06303245d38fe9dc924a4abc48
LeraKulik <57356981+LeraKulik@users.noreply.github.com>
2019-12-11 22:07:11 +0200

    Add files via upload

--- tereshchenkoio/dataset_structure.py
+++ tereshchenkoio/dataset_structure.py
@@ -1,5 +1,20 @@
 dataset = {
-
-    "Bob":17,
-    "Boba":18
+    {
+    'keys' : 'handcraftedalpacablendpurplecharismasweaterperu/19238764,novica/231516,handcraftedalpacablendpurplecharismasweaterperubynovica/19238764',
+    'name' : 'Handcrafted Alpaca Blend "Purple Charisma" Sweater (Peru)',
+    'brand' : 'Novica',
+    'price.size' : 'XL'
+    },
+    {
+    'keys': 'handcraftedalpacablendpurplecharismasweaterperu/19238764,novica/231516,handcraftedalpacablendpurplecharismasweaterperubynovica/19238764',
+    'name': 'Handcrafted Alpaca Blend "Purple Charisma" Sweater (Peru)',
+    'brand': 'Novica',
+    'price.size': 'XL'
+    },
+    {
+        'keys': 'handcraftedalpacablendpurplecharismasweaterperu/19238764,novica/231516,handcraftedalpacablendpurplecharismasweaterperubynovica/19238764',
+        'name': 'Handcrafted Alpaca Blend "Purple Charisma" Sweater (Peru)',
+        'brand': 'Novica',
+        'price.size': 'S'
+    }
 }
